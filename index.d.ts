39a2fac27d231267b922ea3deee3fa4dbad4cbb1
o-jules <jules3in@gmail.com>
2018-09-03 21:40:56 +0800

    add element canvas

--- index.d.ts
+++ index.d.ts
@@ -2247,6 +2247,7 @@ declare namespace wxml {
 
     /**
      * 地图。该组件是原生组件，使用时请注意相关限制。
+     * map 组件使用的经纬度是火星坐标系，调用 wx.getLocation 接口需要指定 type 为 gcj02。
      */
     interface MapElement {
         /**
@@ -2710,6 +2711,59 @@ declare namespace wxml {
         latitude: number;
         longitude: number;
     }
+
+    /**
+     * 画布。该组件是原生组件，使用时请注意相关限制。
+     * 相关api：wx.createCanvasContext。
+     *
+     * 注意：
+     *
+     * - canvas 标签默认宽度300px、高度225px
+     * - 同一页面中的 canvas-id 不可重复，如果使用一个已经出现过的 canvas-id，该 canvas 标签对应的画布将被隐藏并不再正常工作
+     * - bug: 避免设置过大的宽高，在安卓下会有crash的问题
+     */
+    interface CanvasElement {
+        /**
+         * canvas 组件的唯一标识符
+         */
+        canvasId: string;
+
+        /**
+         * 当在 canvas 中移动时且有绑定手势事件时，禁止屏幕滚动以及下拉刷新。
+         * 默认值：false
+         */
+        disableScroll?: boolean;
+
+        /**
+         * 手指触摸动作开始。
+         */
+        bindtouchstart?: EventHandle;
+
+        /**
+         * 手指触摸后移动。
+         */
+        bindtouchmove?: EventHandle;
+
+        /**
+         * 手指触摸动作结束。
+         */
+        bindtouchend?: EventHandle;
+
+        /**
+         * 手指触摸动作被打断，如来电提醒，弹窗。
+         */
+        bindtouchcancel?: EventHandle;
+
+        /**
+         * 手指长按 500ms 之后触发，触发了长按事件后进行移动不会触发屏幕的滚动。
+         */
+        bindlongtap?: EventHandle;
+
+        /**
+         * 当发生错误时触发 error 事件，detail = {errMsg: 'something wrong'}
+         */
+        binderror?: EventHandle;
+    }
 }
 
 declare global {
@@ -2793,6 +2847,8 @@ declare global {
             "live-pusher": wxml.LivePusherElement;
 
             map: wxml.MapElement;
+
+            canvas: wxml.CanvasElement;
         }
     }
 }
