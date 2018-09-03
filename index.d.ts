dedec89e3eb0f6af93de3ee0cc62a88b5110ded7
o-jules <jules3in@gmail.com>
2018-09-03 11:47:38 +0800

    add element input

--- index.d.ts
+++ index.d.ts
@@ -771,6 +771,135 @@ declare namespace wxml {
     }
 
     /**
+     * 输入框。该组件是[原生组件](https://developers.weixin.qq.com/miniprogram/dev/component/native-component.html)，使用时请注意相关限制。
+     */
+    interface InputElement {
+        /**
+         * 输入框的初始内容。
+         */
+        value?: string;
+
+        /**
+         * input 的类型。
+         * 默认值："text"
+         */
+        type?: string;
+
+        /**
+         * 是否是密码类型。
+         * 默认值：false
+         */
+        password?: boolean;
+
+        /**
+         * 输入框为空时占位符。
+         */
+        placeholder?: string;
+
+        /**
+         * 指定 placeholder 的样式。
+         */
+        placeholderStyle?: string;
+
+        /**
+         * 指定 placeholder 的样式类。
+         * 默认值："input-placeholder"
+         */
+        placeholderClass?: string;
+
+        /**
+         * 是否禁用。
+         * 默认值：false
+         */
+        disabled?: boolean;
+
+        /**
+         * 最大输入长度，设置为 -1 的时候不限制最大长度。
+         * 默认值：140
+         */
+        maxlength?: number;
+
+        /**
+         * 指定光标与键盘的距离，单位 px 。取 input 距离底部的距离和 cursor-spacing 指定的距离的最小值作为光标与键盘的距离。
+         * 默认值：0
+         */
+        cursorSpacing?: number;
+
+        /**
+         * (即将废弃，请直接使用 focus )自动聚焦，拉起键盘。
+         * 默认值：false
+         */
+        autoFocus?: boolean;
+
+        /**
+         * 获取焦点。
+         * 默认值：false
+         */
+        focus?: boolean;
+
+        /**
+         * 设置键盘右下角按钮的文字，仅在type='text'时生效。
+         * 默认值："done"
+         * @since 1.1.0
+         */
+        confirmType?: string;
+
+        /**
+         * 点击键盘右下角按钮时是否保持键盘不收起。
+         * 默认值：false
+         * @since 1.1.0
+         */
+        confirmHold?: boolean;
+
+        /**
+         * 指定focus时的光标位置。
+         * @since 1.5.0
+         */
+        cursor?: number;
+
+        /**
+         * 光标起始位置，自动聚集时有效，需与selection-end搭配使用。
+         * 默认值：-1
+         * @since 1.9.0
+         */
+        selectionStart?: number;
+
+        /**
+         * 光标结束位置，自动聚集时有效，需与selection-start搭配使用。
+         * 默认值：-1
+         * @since 1.9.0
+         */
+        selectionEnd?: number;
+
+        /**
+         * 键盘弹起时，是否自动上推页面。
+         * 默认值：true
+         * @since 1.9.90
+         */
+        adjustPosition?: boolean;
+
+        /**
+         * 键盘输入时触发，event.detail = {value, cursor, keyCode}，keyCode 为键值，2.1.0 起支持，处理函数可以直接 return 一个字符串，将替换输入框的内容。
+         */
+        bindinput?: EventHandle;
+
+        /**
+         * 输入框聚焦时触发，event.detail = { value, height }，height 为键盘高度，在基础库 1.9.90 起支持
+         */
+        bindfocus?: EventHandle;
+
+        /**
+         * 输入框失去焦点时触发，event.detail = {value: value}
+         */
+        bindblur?: EventHandle;
+
+        /**
+         * 点击完成按钮时触发，event.detail = {value: value}
+         */
+        bindconfirm?: EventHandle;
+    }
+
+    /**
      * 图片。
      * {@link https://developers.weixin.qq.com/miniprogram/dev/component/image.html | 组件/媒体组件/image}
      */
@@ -875,6 +1004,8 @@ declare global {
             checkbox: wxml.CheckboxElement;
 
             form: wxml.FormElement;
+
+            input: wxml.InputElement;
 
             image: wxml.ImageElement;
         }
