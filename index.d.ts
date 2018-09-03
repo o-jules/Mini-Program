30b11db457f69bd8f97420406d3b5e438ba68a19
o-jules <jules3in@gmail.com>
2018-09-03 11:49:41 +0800

    add element label

--- index.d.ts
+++ index.d.ts
@@ -900,6 +900,18 @@ declare namespace wxml {
     }
 
     /**
+     * 用来改进表单组件的可用性，使用for属性找到对应的id，或者将控件放在该标签下，当点击时，就会触发对应的控件。
+     * for优先级高于内部控件，内部有多个控件的时候默认触发第一个控件。
+     * 目前可以绑定的控件有：<button/>, <checkbox/>, <radio/>, <switch/>。
+     */
+    interface LabelElement {
+        /**
+         * 绑定控件的 id
+         */
+        for: string;
+    }
+
+    /**
      * 图片。
      * {@link https://developers.weixin.qq.com/miniprogram/dev/component/image.html | 组件/媒体组件/image}
      */
@@ -1006,6 +1018,8 @@ declare global {
             form: wxml.FormElement;
 
             input: wxml.InputElement;
+
+            label: wxml.LabelElement;
 
             image: wxml.ImageElement;
         }
