37dcb978a985634d3c7f74f31a5009b12495cafc
o-jules <jules3in@gmail.com>
2018-09-03 15:36:26 +0800

    add element navigator

--- index.d.ts
+++ index.d.ts
@@ -1397,7 +1397,7 @@ declare namespace wxml {
          * 默认值：0
          */
         cursorSpacing: number;
-    
+
         /**
          * 指定focus时的光标位置。
          * @since 1.5.0
@@ -1424,7 +1424,7 @@ declare namespace wxml {
          * @since 1.9.0
          */
         selectionEnd?: number;
-    
+
         /**
          * 键盘弹起时，是否自动上推页面。
          * 默认值：true
@@ -1456,6 +1456,101 @@ declare namespace wxml {
          * 点击完成时， 触发 confirm 事件，event.detail = {value: value}。
          */
         bindconfirm?: EventHandle;
+    }
+
+    /**
+     * 页面链接。
+     */
+    interface NavigatorElement extends Element {
+        /**
+         * 在哪个目标上发生跳转，默认当前小程序。
+         * @since 2.0.7
+         */
+        target?: string;
+
+        /**
+         * 当前小程序内的跳转链接。
+         */
+        url: string;
+
+        /**
+         * 跳转方式。
+         * 默认值：navigate
+         */
+        openType?: string;
+
+        /**
+         * 当 open-type 为 'navigateBack' 时有效，表示回退的层数。
+         */
+        delta?: number;
+
+        /**
+         * 当target="miniProgram"时有效，要打开的小程序 appId。
+         * @since 2.0.7
+         */
+        appId?: string;
+
+        /**
+         * 当target="miniProgram"时有效，打开的页面路径，如果为空则打开首页
+         * @since 2.0.7
+         */
+        path?: string;
+
+        /**
+         * 当target="miniProgram"时有效，需要传递给目标小程序的数据，目标小程序可在 App.onLaunch()，App.onShow() 中获取到这份数据。详情
+         * @since 2.0.7
+         */
+        extraData: any;
+
+        /**
+         * 当target="miniProgram"时有效，要打开的小程序版本，有效值 develop（开发版），trial（体验版），release（正式版），仅在当前小程序为开发版或体验版时此参数有效；如果当前小程序是正式版，则打开的小程序必定是正式版。
+         * 默认值：release
+         * @since 2.0.7
+         */
+        version: "develop" | "release" | "trial";
+
+        /**
+         * 指定点击时的样式类，当hover-class="none"时，没有点击态效果。
+         * 默认值：navigator-hover
+         */
+        hoverClass?: string;
+
+        /**
+         * 指定是否阻止本节点的祖先节点出现点击态。
+         * 默认值：false
+         * @since 1.5.0
+         */
+        hoverStopPropagation?: boolean;
+
+        /**
+         * 按住后多久出现点击态，单位毫秒。
+         * 默认值：50
+         */
+        hoverStartTime?: number;
+    
+        /**
+         * 手指松开后点击态保留时间，单位毫秒。
+         * 默认值：600
+         */
+        hoverStayTime?: number;
+
+        /**
+         * 当target="miniProgram"时有效，跳转小程序成功。
+         * @since 2.0.7
+         */
+        bindsuccess?: string;
+    
+        /**
+         * 当target="miniProgram"时有效，跳转小程序失败。
+         * @since 2.0.7
+         */
+        binderror?: string;
+    
+        /**
+         * 当target="miniProgram"时有效，跳转小程序完成。
+         * @since 2.0.7
+         */
+        bindcomplete?: string;
     }
 
     /**
@@ -1583,6 +1678,8 @@ declare global {
             switch: wxml.SwitchElement;
 
             textarea: wxml.TextareaElement;
+
+            navigator: wxml.NavigatorElement;
 
             image: wxml.ImageElement;
         }
